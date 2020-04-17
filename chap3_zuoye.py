4a972c61e53e40d343674e2817e0daa1d943e631
FancyKuang <249193989@qq.com>
2020-04-17 21:30:58 +0800

    alter

--- chap3_zuoye.py
+++ chap3_zuoye.py
@@ -21,3 +21,28 @@ if __name__ == "__main__":
         t.start()
     for thread in threads:
         thread.join()
+    
+    
+# 作业2：使用多进程写一个并发http，get请求的程序
+# 可设置并发数和请求总数，返回请求状态码
+from multiprocessing import Process
+import requests
+count = 5
+numprocess = 10
+
+def httprequest(url):
+    for i in range(count):
+        r = requests.get(url)
+        print(r.status_code)
+
+if __name__ =="__main__":
+    url = "https://www.baidu.com"
+    processes = []
+    for i in range(numprocess):
+        p = Process(target=httprequest,args=(url,))
+        processes.append(p)
+        p.daemon = True
+        p.start()
+
+    for p in processes:
+        p.join()
